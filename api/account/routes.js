fd0dd4eb71f8b5a9df5cd5ff75bb79c35006afc0
abcd123 <ghaninf@gmail.com>
2023-06-12 19:53:54 +0700

    reset api

--- api/account/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller');
-
-router.post(
-  '/login',
-  controller.login
-);
-
-module.exports = router;
--- api/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  url: process.env.DB_URI,
-  database: process.env.DB_IMAGE,
-};
